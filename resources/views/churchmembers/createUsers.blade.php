26ad7369436afb3c115084dd73148511a46e4976
Josiah Ekow Walker <chokolayte@gmail.com>
2017-10-25 12:59:04 +0100

    CMWeb
    Original Church Manager in Laravel

---
+++ resources/views/churchmembers/createUsers.blade.php
@@ -0,0 +1,53 @@
+<div id="container" style="width: 600px;margin: 10px auto;">
+
+    <div class="form-group row">
+        <label for="lastName" class="col-2 col-form-label">Last Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="" id="lastName">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="firstName" class="col-2 col-form-label">First Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="" id="firstName">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="otherName" class="col-2 col-form-label">Other Name</label>
+        <div class="col-10">
+            <input class="form-control" type="text" value="" id="otherName">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="emailAddress" class="col-2 col-form-label">Email</label>
+        <div class="col-10">
+            <input class="form-control" type="email" value="" id="emailAddress">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="telephoneNumber" class="col-2 col-form-label">Telephone Number</label>
+        <div class="col-10">
+            <input class="form-control" type="tel" value="" id="telephoneNumber">
+        </div>
+
+    </div>
+    <div class="form-group row">
+        <label for="password" class="col-2 col-form-label">Password</label>
+        <div class="col-10">
+            <input class="form-control" type="password" value="" id="password">
+        </div>
+    </div>
+    <div class="form-group row">
+        <label for="DOB" class="col-2 col-form-label">Date of Birth</label>
+        <div class="col-10">
+            <input class="form-control" type="date" value="" id="DOB">
+        </div>
+    </div>
+
+    <div class="form-group row">
+        <button type="button" class="btn btn-primary btn-lg" style="margin-left: 400px">Submit Form</button>
+
+    </div>
+</div>
+
+
